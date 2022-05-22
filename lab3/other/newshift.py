0835b7464fa3e53e304850a11396a185bb3fdfc8
Jakub Góralczyk <s18327@users.noreply.github.com>
2022-05-22 22:55:22 +0200

    initial commit

---
+++ lab3/other/newshift.py
@@ -0,0 +1,12 @@
+import numpy as np
+import cv2 as cv
+
+print("OpenCV version :  {0}".format(cv.__version__))
+img = cv.imread('saw1.jpg')
+gray= cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+sift = cv.SIFT_create()
+kp = sift.detect(gray,None)
+img=cv.drawKeypoints(gray,kp,img)
+cv.imwrite('sift_keypoints.jpg',img)
+img=cv.drawKeypoints(gray,kp,img,flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+cv.imwrite('sift_keypoints.jpg',img)
